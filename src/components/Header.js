add2594587676d1ddf2f890209240f1c0492fa5e
Jose <jlcab8@gmail.com>
2022-04-25 00:57:52 +0200

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Header.css";
+import punkLogo from "./../assets/header/cryptopunk-logo.png";
+import searchIcon from "./../assets/header/search.png";
+import themeSwitch from "./../assets/header/theme-switch.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="Punk Logo Image"></img>
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} className="searchIcon" alt="Search Icon" />
+        </div>
+        <input
+          className="searchInput"
+          type="text"
+          placeholder="Collection, item, user ..."
+        />
+      </div>
+
+      <div className="headerItemsContainer">
+        <p class="headerItem">Drops</p>
+        <p class="headerItem">Marketplace</p>
+        <p class="headerItem">Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img
+            src={themeSwitch}
+            className="themeSwitch"
+            alt="Theme Switch Icon"
+          ></img>
+        </div>
+      </div>
+
+      <div class="loginButton">GET IN</div>
+    </div>
+  );
+};
+
+export default Header;
